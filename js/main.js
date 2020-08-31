396ccbcfc045edb02d7754ec2890d99611979271
Rodrigo Padilla <padillarodrigo2@gmail.com>
2020-08-31 12:22:41 -0400

    Se agregaron lo referente a Notes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $(".tasks").DataTable({});
+
+$(".notes").DataTable({});
+
 $("#add").click(function(){
     $("#new").show(500);
     $("#add").hide(500);
 });
+
 $("#close").click(function(){
     $("#new").hide(500);
     $("#add").show(500);
+});
+
+$("#add1").click(function(){
+    $("#new1").show(500);
+    $("#add1").hide(500);
+});
+
+$("#close1").click(function(){
+    $("#new1").hide(500);
+    $("#add1").show(500);
 });
